aab4ff136a7f4394c7c70c13498c64e8a14897ed
Anirudh Singh <64956321+anirudhsingh05@users.noreply.github.com>
2024-08-09 18:51:39 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-# Update package list
-echo "Updating package list..."
-sudo apt update
-
-# Install Ruby and other packages
-echo "Installing Ruby, wget, and unzip..."
-sudo apt install -y ruby-full wget unzip
-
-# Install default JRE
-echo "Installing default JRE..."
-sudo apt install -y default-jre
-
-# Install OpenJDK 17
-echo "Installing OpenJDK 17..."
-sudo apt install -y openjdk-17-jdk
-
-# Install AWS CLI
-echo "Installing AWS CLI..."
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
-# Install unzip utility
-echo "Installing unzip utility..."
-sudo apt install -y unzip
+java --version
 
 echo "Dependencies installed successfully."
