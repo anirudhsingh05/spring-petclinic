aad4612f5f3155f4cfcbcfe6b4ad3f8efc98a0a4
Anirudh Singh <64956321+anirudhsingh05@users.noreply.github.com>
2024-08-09 18:57:10 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-# Find and kill the Java process
-echo "Stopping the server..."
-pkill -f "java -jar spring-petclinic-3.3.0-SNAPSHOT.jar"
 
-# Optional: Confirm server has stopped
-echo "Checking server status..."
-curl -I localhost:8080 || echo "Server stopped."
-
-echo "Server stopped successfully."
