2bbd8469437eaa5cd02bd21108adcfc99977db11
Anirudh Singh <64956321+anirudhsingh05@users.noreply.github.com>
2024-08-09 18:53:56 +0530

    Update set_permissions.sh

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -4,8 +4,4 @@
 echo "Setting permissions for AWS CodeDeploy install script..."
 chmod +x /home/ubuntu/install
 
-# Ensure executable permission for any other necessary files
-echo "Setting permissions for other files..."
-chmod +x /path/to/other/files
-
 echo "Permissions set successfully."
