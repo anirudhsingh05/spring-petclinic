00abb146b29a3dd12ec12301e9d0ad5458551df6
Anirudh Singh <64956321+anirudhsingh05@users.noreply.github.com>
2024-08-09 18:12:30 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-./setup_script.sh
+# Change to the application directory
+echo "Changing directory to /home/ubuntu/target..."
+cd /home/ubuntu/target/
 
+# Start the application server
+echo "Starting the server..."
+java -jar spring-petclinic-3.3.0-SNAPSHOT.jar &
 
+# Optional: Check if the server started successfully
+echo "Checking server status..."
+curl -I localhost:8080
+
+echo "Server started successfully."
