63d9a26ada771a62f7f3f852d741481568636e04
Anirudh Singh <64956321+anirudhsingh05@users.noreply.github.com>
2024-08-09 17:52:21 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Echo a message for logging
-echo "Starting Apache server"
+./setup_script.sh
 
-# Start the Apache service
-sudo systemctl start apache2
-
-# Check if the Apache service started successfully
-if systemctl is-active --quiet apache2; then
-    echo "Apache is running"
-    exit 0
-else
-    echo "Apache failed to start"
-    exit 1
-fi
 
