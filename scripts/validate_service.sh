cd63ff09ae7cc2afb6ccb6a4de29edaa6f3c3800
Anirudh Singh <64956321+anirudhsingh05@users.noreply.github.com>
2024-08-12 17:39:14 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -2,7 +2,7 @@
 
 # Check if the server is running
 echo "Validating the service..."
-curl -I localhost:8080 | grep "200 OK"
+curl -I 15.206.127.73:8080 | grep "200 OK"
 
 # Optionally check if the process is running
 echo "Checking if the Java process is running..."
