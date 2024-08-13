3df62ad883385c7e72dbfe206049ac4835347e63
Anirudh Singh <64956321+anirudhsingh05@users.noreply.github.com>
2024-08-13 10:47:26 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-check = $(ps aux | grep spring-petclinic-3.3.0-SNAPSHOT.jar)
+# Check if the application is running
+check=$(ps aux | grep '[s]pring-petclinic-3.3.0-SNAPSHOT.jar')
 
-if [[$check -eq 0]]; then
+# If check is not empty, the application is running
+if [ -n "$check" ]; then
     echo "Application is running"
     pkill -f spring-petclinic-3.3.0-SNAPSHOT.jar
     exit 0
 else
-    echo "failed to stop"
+    echo "Application failed to stop or is not running"
     exit 1
 fi
